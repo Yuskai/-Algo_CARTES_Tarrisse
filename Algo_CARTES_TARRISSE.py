713cf2bcb78ef2851edef54d9c91f85f64f13461
Tarrisse.Allan <93705557+Yuskai@users.noreply.github.com>
2022-05-06 13:01:36 +0200

    Add files via upload

---
+++ Algo_CARTES_TARRISSE.py
@@ -0,0 +1,12 @@
+from multiprocessing import managers
+from unicodedata import name
+
+
+class cartes 
+delf__init__(self,mana,name,description)
+
+self.mana = mana
+self.name = name
+self.description = description
+
+class
